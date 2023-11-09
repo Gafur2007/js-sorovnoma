163d2f184254b686857c53bc71dc01fe52af2a68
Gafur2007 <tohirovabdugofur@gmail.com>
2023-11-09 15:49:31 +0300

    sorovnoma

---
+++ main/main.js
@@ -0,0 +1,14 @@
+var firstname = String(prompt("Ismingizni kiriting"));
+var lastname = String(prompt("Familyangizni kiriting"));
+var fullname = String(prompt("Sharifingizni kiriting"));
+var telnumber = Number(prompt("Telefon raqamingizni kiriting"));
+var live = String(prompt("Turar joyingizni kiriting"));
+var job = String(prompt("Kasbingizni kiriting"));
+var tel = String(prompt("Telefoningiz rusumini kiriting"));
+var bestfriend = String(prompt("Yaqin dostingizni ismini kiriting"));
+var girlfriendnumber = Number(prompt("Gaplawgan qizlaringiz sinini kiriting"));
+var girlfriend = String(prompt("Sevgan qizingizni ismini kiriting"));
+
+alert(firstname + lastname + fullname + telnumber + live + job + tel + bestfriend + girlfriendnumber + girlfriend) = toliqmalumot;
+
+console.log(toliqmalumot);
